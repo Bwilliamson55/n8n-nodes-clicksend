407b87e4bb5ef06771e3d524630bb3a8bb39baa2
bwilliamson55 <benjaminawilliamson@gmail.com>
2023-04-08 15:31:16 -0400

    Add Account Usage method

--- nodes/ClickSend/AccountDescription.ts
+++ nodes/ClickSend/AccountDescription.ts
@@ -1,7 +1,6 @@
 import type {
 	INodeProperties,
 } from 'n8n-workflow';
-// import { SendInBlueNode } from './GenericFunctions';
 
 export const accountOperations: INodeProperties[] = [
 	{
@@ -16,7 +15,7 @@ export const accountOperations: INodeProperties[] = [
 		},
 		options: [
 			{
-				name: 'Get',
+				name: 'Get Account Details',
 				value: 'get',
 				action: 'Get account details',
 				description: 'Get the details of the account',
@@ -27,7 +26,55 @@ export const accountOperations: INodeProperties[] = [
 					},
 				},
 			},
+			{
+				name: 'Get Account Usage',
+				value: 'get_usage',
+				action: 'Get account usage',
+				description: 'Get the usage of the account',
+				routing: {
+					request: {
+						method: 'GET',
+						url: '=/account/usage/{{$parameter.year}}/{{$parameter.month}}/subaccount',
+					},
+				},
+			},
 		],
 		default: 'get',
 	},
+];
+
+const accountUsageOperationFields: INodeProperties[] = [
+	{
+		displayName: 'Year',
+		name: 'year',
+		type: 'number',
+		default: 2023,
+		displayOptions: {
+			show: {
+				resource: ['account'],
+				operation: ['get_usage'],
+			},
+		},
+		required: true,
+		description: 'Year of usage',
+	},
+	{
+		displayName: 'Month',
+		name: 'month',
+		type: 'number',
+		default: 3,
+		displayOptions: {
+			show: {
+				resource: ['account'],
+				operation: ['get_usage'],
+			},
+		},
+		required: true,
+		description: 'Month of usage as a number - eg March is 3',
+	},
+];
+
+export const accountFields: INodeProperties[] = [
+	// Get usage
+	...accountUsageOperationFields,
 ];
